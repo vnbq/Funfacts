0ba8175e3d1cce6c33a9f68b7c7dd74bb2857203
vnbq <foxysroadtech@gmail.com>
2024-09-13 15:29:30 -0500

    Add files via upload

---
+++ Funfacts.py
@@ -0,0 +1,34 @@
+print("Fun fact:")
+
+import random
+Word=random.randint(1,6)
+
+if Word == 1:
+   {
+  print("This project is open source!")   
+}
+   
+if Word == 2:
+   {
+  print("The first browser to ever exist was called mosaic!")   
+}
+   
+if Word == 3:
+   {
+  print("The Linux Penguin is called Tux!")   
+}
+   
+if Word == 4:
+   {
+  print("This was coded in python!")   
+}
+
+if Word == 5:
+   {
+  print("There actually was a Windows 9 Release!")   
+}
+  
+if Word == 6:
+   {
+  print("Currently, Linux holds 4.4% of the OS market shart!")   
+}
